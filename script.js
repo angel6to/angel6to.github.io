cbb773ae1d353e62d75ffda769ee1ea818104ded
angel6to <angelmensajesapp@gmail.com>
2025-06-25 03:16:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let clickCount = 0;
+const giftBox = document.getElementById("giftBox");
+const message = document.getElementById("message");
+
+// Cambia esto por la URL sorpresa
+const finalURL = "https://angel4to.github.io";
+
+const messages = [
+  "⚠️¡GIRA LA PANTALA EN HORIZOTAL⚠️!",
+  "¡Qué emoción!",
+  "¿Preparado para la sorpresa?",
+  "¡Ya casi llegas!",
+  "Solo un poco más...",
+  "¡Último clic!"
+];
+
+giftBox.addEventListener("click", () => {
+  clickCount++;
+
+  // Animación de rebote
+  giftBox.style.transform = "scale(1.2)";
+  setTimeout(() => {
+    giftBox.style.transform = "scale(1)";
+  }, 150);
+
+  if (clickCount <= messages.length) {
+    message.textContent = messages[clickCount - 1];
+  }
+
+  if (clickCount > messages.length) {
+    window.location.href = finalURL;
+  }
+});
